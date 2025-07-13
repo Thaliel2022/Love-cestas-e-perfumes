2acc1c120a66c3e022db03e5407bf507acbdba07
Thaliel2022 <thaliel1994@hotmail.com>
2025-07-13 04:14:27 -0300

    12

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -4829,7 +4829,7 @@ export default function App() {
 
         const manifestContent = {
             "name": APP_NAME,
-            "short_name": "Love Cestas e Perfumes",
+            "short_name": "Love Cestas",
             "description": "Sua loja de cestas e perfumes.",
             "start_url": "/",
             "display": "standalone",
