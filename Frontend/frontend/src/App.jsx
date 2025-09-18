4d2f948792f6564c42169d2abb0846d6e6f04da7
Thaliel2022 <thaliel1994@hotmail.com>
2025-09-18 20:34:02 -0300

    12
    12

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -924,18 +924,20 @@ const ProductCard = memo(({ product, onNavigate }) => {
                 )}
             </div>
             <div className="p-5 flex flex-col flex-grow">
-                <div>
+                <div className="min-h-24">
                     <p className="text-xs text-amber-400 font-semibold tracking-wider">{product.brand.toUpperCase()}</p>
-                    <h4 className="text-xl font-bold tracking-wider mt-1 cursor-pointer hover:text-amber-400 min-h-[3.5rem]" onClick={() => onNavigate(`product/${product.id}`)}>{product.name}</h4>
+                    <h4 className="text-xl font-bold tracking-wider mt-1 cursor-pointer hover:text-amber-400" onClick={() => onNavigate(`product/${product.id}`)}>{product.name}</h4>
                     
                     <div className="flex items-center mt-2 h-5">
-                        {[...Array(5)].map((_, i) => (
-                            <StarIcon 
-                                key={i} 
-                                className={`h-5 w-5 ${i < avgRating ? 'text-amber-400' : 'text-gray-600'}`} 
-                                isFilled={i < avgRating}
-                            />
-                        ))}
+                        {hasRating && (
+                            [...Array(5)].map((_, i) => (
+                                <StarIcon 
+                                    key={i} 
+                                    className={`h-5 w-5 ${i < avgRating ? 'text-amber-400' : 'text-gray-600'}`} 
+                                    isFilled={i < avgRating}
+                                />
+                            ))
+                        )}
                     </div>
                 </div>
                 
