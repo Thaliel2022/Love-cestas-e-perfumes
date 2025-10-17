223ce87e0fe82780c56604a3e222a0388215d494
Thaliel2022 <thaliel1994@hotmail.com>
2025-10-17 00:33:37 -0300

    Update App.jsx

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -190,7 +190,7 @@ async function apiService(endpoint, method = 'GET', body = null, options = {}) {
 
 async function apiUploadService(endpoint, file) {
     const formData = new FormData();
-    formData.append('image', file); // CORRIGIDO: de 'file' para 'image'
+    formData.append('file', file);
 
     const config = {
         method: 'POST',
